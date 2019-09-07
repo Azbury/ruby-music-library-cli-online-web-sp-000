b76d7362610070dc1b5de1a73dbb8d7f5f33795b
Austin Asbury <acasbury25@aol.com>
2019-09-07 00:49:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,18 @@
 class Artist
   attr_accessor :name
+  attr_reader :songs
+  
   @@all = []
 
   def initialize (name)
     @name = name
+    @songs = []
   end
 
+  def songs
+    self.songs
+  end
+  
   def self.all
     @@all
   end
