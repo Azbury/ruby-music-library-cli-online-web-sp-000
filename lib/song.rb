cb5611ada28a31258de48662c1e6d1cd5edbd139
Austin Asbury <acasbury25@aol.com>
2019-09-07 03:41:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   end
 
   def self.new_from_filename (filename)
-    filename_array.split(" - ")
+    filename_array = filename.split(" - ")
     Song.new(filename_array[1], filename_array[0], filename_array[2])
   end
   def self.find_by_name(name)
