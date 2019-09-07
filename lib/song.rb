4e77a9e2f9d1c9863d129dc28e85c30583358928
Austin Asbury <acasbury25@aol.com>
2019-09-07 03:41:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,10 @@ class Song
     self.genre = genre if genre != nil
   end
 
+  def self.new_from_filename (filename)
+    filename_array.split(" - ")
+    Song.new(filename_array[1], filename_array[0], filename_array[2]) 
+  end
   def self.find_by_name(name)
     self.all.detect{|s| s.name == name}
   end
